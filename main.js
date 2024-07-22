5ce62b39fe5cae67f29871fea872e8dc985b685e
victor <victorosinachi11@gmail.com>
2024-07-22 05:38:05 +0100

    Added some images and worked on the testimonial section

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ const body = document.querySelector('body');
 const service = document.querySelector('#services');
 const subtitle = document.querySelectorAll('.subtitle');
 const agentBgColor = document.querySelector('#agent');
+const testimonialsBgColor = document.querySelector('#testimonials');
+const activePic = document.querySelector('.active-pic');
+
+
 
 function changeTheme(){
     document.body.classList.toggle('dark-theme');
@@ -30,13 +34,16 @@ function changeTheme(){
         icon.src = "images/lightmode.png";
         body.style.transition = '1.5s';
         body.style.background = '#ebebeb';
+        testimonialsBgColor.style.background = '#ebebeb';
         service.style.background = '#d3d3d3';
-        subtitle.style.color = '#000';
+        subtitle.style.color = '#000000';
         agentBgColor.style.background = '#ebebeb';
+        activePic.style.border = '#ffc107'
     } else{
         icon.src = "images/darkmode.png";
         body.style.transition = '1.5s';
         body.style.background = '#fff';
+        testimonialsBgColor.style.background = '#fff';
         service.style.background = '#d3d3d3';
         subtitle.style.color = '#fff';
         agentBgColor.style.background = '#1292d5';
@@ -44,3 +51,20 @@ function changeTheme(){
 }
 
     icon.addEventListener('click', changeTheme);
+
+
+    let userTexts = document.getElementsByClassName('user-text');
+    let userPics = document.getElementsByClassName('user-pic');
+
+    function showReview() {
+        for(userPic of userPics) {
+            userPic.classList.remove('active-pic');
+    }
+    for(userText of userTexts) {
+        userText.classList.remove('active-text');
+    
+        let i = Array.from(userPics).indexOf(event.target);
+        userPics[i].classList.add('active-pic');
+        userTexts[i].classList.add('active-text');
+}
+    }
