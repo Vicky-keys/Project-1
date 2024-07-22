94205e958df224742319ebfb37b3cdb56f850a44
victor <victorosinachi11@gmail.com>
2024-07-22 01:26:15 +0100

    Added the javascript functions for the sidebar and dark theme toggle

---
+++ main.js
@@ -0,0 +1,46 @@
+const sideBar = document.getElementById("sidebar");
+const menuBtn = document.getElementById("hamburger");
+sideBar.style.left = "-250px";
+
+function toggle(){
+    if(sideBar.style.left == "-250px"){
+        sideBar.style.left = "0";
+        menuBtn.src = "images/close.png"
+    } 
+    else{
+        sideBar.style.left = "-250px";
+        menuBtn.src = "images/menu.png";   
+    } 
+    
+}
+
+menuBtn.addEventListener("click", toggle);
+
+
+const icon = document.getElementById('icon');
+const heroColor = document.querySelector('.hero-section');
+const body = document.querySelector('body');
+const service = document.querySelector('#services');
+const subtitle = document.querySelectorAll('.subtitle');
+const agentBgColor = document.querySelector('#agent');
+
+function changeTheme(){
+    document.body.classList.toggle('dark-theme');
+    if(document.body.classList.contains('dark-theme')){
+        icon.src = "images/lightmode.png";
+        body.style.transition = '1.5s';
+        body.style.background = '#ebebeb';
+        service.style.background = '#d3d3d3';
+        subtitle.style.color = '#000';
+        agentBgColor.style.background = '#ebebeb';
+    } else{
+        icon.src = "images/darkmode.png";
+        body.style.transition = '1.5s';
+        body.style.background = '#fff';
+        service.style.background = '#fff';
+        subtitle.style.color = '#fff';
+        agentBgColor.style.background = '#1292d5';
+    }
+}
+
+    icon.addEventListener('click', changeTheme);
